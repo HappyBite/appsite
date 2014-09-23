8767c163338e13e75191317be9d869631349af52
Gabriel Lantz <lantzgabriel@gmail.com>
2014-09-24 00:26:19 +0200

    asdfa:wq :wq

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var session = require('express-session')
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -10,6 +11,8 @@ var users = require('./routes/users');
 
 var app = express();
 
+//var helpers = require('express-helpers')();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -20,10 +23,30 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+    secret: 'secret',
+    name: 'my_cookie',
+    //store: sessionStore, // connect-mongo session store
+    proxy: false,
+    resave: true,
+    saveUninitialized: true
+}));
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Helpers
+app.use(function(req, res, next) {
+	// You need to tell your templates about your function
+	// which is done by passing your function to res.locals
+	res.locals.truncate_words = function(html, words) {
+       return html.split(/\s/).slice(0, words).join(' ');
+    }
+	next();
+});
+
 app.use('/', routes);
+app.use('/posts', routes);
+app.use('/posts/post', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
@@ -56,6 +79,5 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
 
 module.exports = app;
