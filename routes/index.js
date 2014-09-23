11900c81ac9796fdf0ba6b70e1519018a3e502ce
Gabriel Lantz <lantzgabriel@gmail.com>
2014-09-24 00:36:23 +0200

    General

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
-var router = express.Router();
+var app = express();
+var client = require('../lib/rest');
+var navigation = require('../snippets/navigation');
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+/* Get home page. */
+app.get('/', function(req, res) {
+	res.render('index', { navigation: navigation.Get(req), title: 'Home' });
 });
 
-module.exports = router;
+/* Get blog posts page. */
+app.get('/posts', function(req, res) {
+	client.Init(req, function(auth) {
+		client.Get('/v1/sites/90/posts/blog-post', {}, function(data) {
+			res.render('posts', { navigation: navigation.Get(req), title: 'Blog', posts: data });
+		});
+	});
+});
+
+/* Get single post page. */
+app.get('/posts/post/:id', function(req, res) {
+	client.Init(req, function(auth) {
+		client.Get('/v1/sites/90/posts/blog-post/'+req.params.id, {}, function(data) {
+			res.render('post', { navigation: navigation.Get(req), title: 'Blog', post: data });
+		});
+	});
+});
+
+module.exports = app;
